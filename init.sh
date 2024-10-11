b31677ec3ee97aa434c718b35d09994d70207e64
zhibin.chen <2351103450@qq.com>
2024-10-11 23:40:57 +0800

    change config

--- init.sh
+++ init.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+dotfiles_dir=$(pwd -P)
+
+echo $dotfiles_dir
+
 # hyprland
 mkdir -p ~/.config/hypr
-cp -R ./hyprland/* ~/.config/hypr
+rm -rf ~/.config/hypr/*
+cp -R $dotfiles_dir/hyprland/* ~/.config/hypr
+
+# hyprpaper
+mkdir -p ~/.config/hypr
+cp -R $dotfiles_dir/hyprpaper/* ~/.config/hypr
 
 # waybar
 mkdir -p ~/.config/waybar
-cp -R ./waybar/* ~/.config/waybar
+cp -R $dotfiles_dir/waybar/* ~/.config/waybar
